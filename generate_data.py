70eb2bfaebcaad6672ccee3426ae7be4ed5794d0
Giorgi Kldiashvili <giorgik@mit.edu>
2023-05-08 13:57:49 -0400

    Modified filename format for frames and audio files.

--- generate_data.py
+++ generate_data.py
@@ -11,12 +11,10 @@ def format_timedelta(td):
     omitting microseconds and retaining milliseconds"""
     result = str(td)
     try:
-        result, ms = result.split(".")
+        result, _ = result.split(".")
     except ValueError:
         return (result + ".00").replace(":", "-")
-    ms = int(ms)
-    ms = round(ms / 1e4)
-    return f"{result}.{ms:02}".replace(":", "-")
+    return f"{result}".replace(":", "-")
 
 
 def get_saving_frames_durations(cap, saving_fps):
